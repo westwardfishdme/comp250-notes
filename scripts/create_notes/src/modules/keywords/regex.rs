eb2daa0ef4d151cc183511150ad9ff599ca1e526
withoutdishonor <westwardfishdme@gmail.com>
2026-09-23 03:39:57 -0500

    move capitalize to utils

--- scripts/create_notes/src/modules/keywords/regex.rs
+++ scripts/create_notes/src/modules/keywords/regex.rs
@@ -1,25 +1,9 @@
 use std::{collections::HashSet, error::Error, fs, path::PathBuf};
 
+use crate::modules::utils::capitalize::Capitalize;
 use regex::Regex;
 
 const REGEX_STR: &str = "^(\\*\\*Keywords:\\*\\*)";
-trait Capitalize {
-    fn capitalize(self) -> String;
-}
-impl<T: ToString> Capitalize for T {
-    fn capitalize(self) -> String {
-        let mut s = self
-            .to_string()
-            .as_str()
-            .trim()
-            .chars()
-            .collect::<Box<[char]>>();
-        let cap = s[0].to_uppercase().next().expect("no char");
-
-        s[0] = cap;
-        s.iter().collect::<String>()
-    }
-}
 
 pub fn find_keywords(
     file: PathBuf,
